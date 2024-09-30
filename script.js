971dcbabbfe468a93579cb0e391abef80f064794
sujipinku07 <sujipinku07@gmail.com>
2024-09-30 19:20:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+    // Convert both strings to lower case for case insensitive comparison
+    s1 = s1.toLowerCase();
+    s2 = s2.toLowerCase();
+
+    // Check if s2 is included in s1
+    if(!s1.includes(s2)){
+        return -1;
+    }
+
+    // Return the index of s2 in s1
+    return s1.indexOf(s2);
 }
 
-// Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+alert(indexOfIgnoreCase(s1, s2));
